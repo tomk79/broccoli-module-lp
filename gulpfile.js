0f78c1e371cd716e5680954462f63ff9522c9211
Tomoya Koyanagi <tomk79@gmail.com>
2020-05-16 14:36:08 +0900

    update gulp, and libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
-var gulp = require('gulp');
-var plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
-var concat  = require('gulp-concat');
-var browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
-var _tasks = [
-	'carousel'
-];
+let gulp = require('gulp');
+let plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
+let concat  = require('gulp-concat');
+let browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
 
 // src 中の *.js を処理
-gulp.task('carousel', function(){
-	gulp.src(["./src_gulp/fields/cssMarginPadding/frontend.js"])
+gulp.task('carousel:js', function(){
+	return gulp.src(["./src_gulp/fields/cssMarginPadding/frontend.js"])
 		.pipe(plumber())
 		.pipe(browserify({}))
 		.pipe(concat('frontend.js'))
 		.pipe(gulp.dest( './fields/cssMarginPadding/frontend/' ))
 	;
-	gulp.src(["./src_gulp/fields/cssMarginPadding/frontend.css"])
+});
+gulp.task('carousel:css', function(){
+	return gulp.src(["./src_gulp/fields/cssMarginPadding/frontend.css"])
 		.pipe(plumber())
 		.pipe(concat('frontend.css'))
 		.pipe(gulp.dest( './fields/cssMarginPadding/frontend/' ))
 	;
 });
 
+
+
+let _tasks = gulp.parallel(
+	'carousel:js',
+	'carousel:css'
+);
+
 // src 中のすべての拡張子を監視して処理
 gulp.task("watch", function() {
-	gulp.watch(["src_gulp/**/*"], _tasks);
+	return gulp.watch(["src_gulp/**/*"], _tasks);
 });
-
 
 // src 中のすべての拡張子を処理(default)
 gulp.task("default", _tasks);
